63153d2a91b734ede67022408379c553844ea73b
asthas0108 <asthas0108@gmail.com>
2025-07-12 16:14:40 +0530

    add profile default pic

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   points: { type: Number, default: 0 },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  avatar: { type: String, default: 'https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg' }
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
