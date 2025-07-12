5eb62e8bf90612264695eeb2e5dd0a6beda8f619
Manya Rawat <manyarawat2005@gmail.com>
2025-07-12 12:47:50 +0530

    update admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import Footer from './components/Footer';
 import Login from './pages/LoginPage';
 import Signup from './pages/RegisterPage';
+import {Admin} from './pages/Admin';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer/>
     </Router>
