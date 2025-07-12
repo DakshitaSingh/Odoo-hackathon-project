66ee683040f79c47e17fb186105661037933b045
manya014 <159939920+manya014@users.noreply.github.com>
2025-07-12 11:14:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import AddNewItemPage from './pages/AddNewItemPage';
+import {Admin} from "./pages/Admin"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path='/product' element={<ProductDetailPage/>}/>
         <Route path='/add' element={<AddNewItemPage/>}/>
+        <Route path='/admin' element={<Admin/>}/>
         {/* Add more routes here as you build other pages */}
       </Routes>
     </Router>
